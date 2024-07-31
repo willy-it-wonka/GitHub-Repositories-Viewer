2073a39f53133efd940bea2eab21d9f994a784de
Willy <willy.it.wonka@outlook.com>
2024-07-31 21:34:39 +0200

    Initial commit of project files

---
+++ src/main/java/com/github/repos/viewer/GitHubRepositoriesViewerApplication.java
@@ -0,0 +1,13 @@
+package com.github.repos.viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRepositoriesViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubRepositoriesViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/repos/viewer/GitHubRepositoriesViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.repos.viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepositoriesViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
