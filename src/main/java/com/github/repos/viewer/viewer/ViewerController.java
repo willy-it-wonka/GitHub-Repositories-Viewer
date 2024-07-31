ae15c608fc31780d59bc5fa337bae9926b8c8f33
Willy <willy.it.wonka@outlook.com>
2024-07-31 22:34:35 +0200

    Define endpoint to retrieve user repositories

--- src/main/java/com/github/repos/viewer/viewer/ViewerController.java
+++ src/main/java/com/github/repos/viewer/viewer/ViewerController.java
@@ -1,4 +1,23 @@
 package com.github.repos.viewer.viewer;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class ViewerController {
+
+    private final ViewerService viewerService;
+
+    public ViewerController(ViewerService viewerService) {
+        this.viewerService = viewerService;
+    }
+
+    @GetMapping("/{username}/repos")
+    public void getUserRepositories(@PathVariable String username) {
+        //TODO: call method from ViewerService. Return ViewerResponse.
+    }
+
 }
