c3ac6a5f7524e9a0f817556e39f52907b4708606
Willy <willy.it.wonka@outlook.com>
2024-08-01 19:34:50 +0200

    Implement GitHubApiResponse to handle data structure from GitHub API
    Class nesting is a mapping of data nesting in JSON format. See:
    https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-a-user
    https://api.github.com/users/willy-it-wonka/repos
    https://api.github.com/repos/willy-it-wonka/Bookshelf-backend/branches

--- src/main/java/com/github/repos/viewer/viewer/payload/GitHubApiResponse.java
+++ src/main/java/com/github/repos/viewer/viewer/payload/GitHubApiResponse.java
@@ -1,4 +1,33 @@
 package com.github.repos.viewer.viewer.payload;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class GitHubApiResponse {
+    private String name;
+    private Owner owner;
+    private List<GitHubBranch> branches;
+    private boolean fork;
+
+    @JsonProperty("branches_url")
+    private String branchesUrl;
+
+    @Data
+    public static class Owner {
+        private String login;
+    }
+
+    @Data
+    public static class GitHubBranch {
+        private String name;
+        private Commit commit;
+
+        @Data
+        public static class Commit {
+            private String sha;
+        }
+    }
 }
