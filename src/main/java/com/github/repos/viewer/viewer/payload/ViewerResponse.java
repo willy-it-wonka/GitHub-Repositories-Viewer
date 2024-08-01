e91ef95bfafa8515d534b43328831bc337c8f319
Willy <willy.it.wonka@outlook.com>
2024-08-01 19:53:00 +0200

    Implement ViewerResponse to handle responses for client

--- src/main/java/com/github/repos/viewer/viewer/payload/ViewerResponse.java
+++ src/main/java/com/github/repos/viewer/viewer/payload/ViewerResponse.java
@@ -1,4 +1,23 @@
 package com.github.repos.viewer.viewer.payload;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class ViewerResponse {
+    private String name;
+    private Owner owner;
+    private List<Branch> branches;
+
+    @Data
+    public static class Owner {
+        private String login;
+    }
+
+    @Data
+    public static class Branch {
+        private String name;
+        private String lastCommitSha;
+    }
 }
